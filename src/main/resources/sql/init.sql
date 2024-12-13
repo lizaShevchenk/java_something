f5380227fabc974be8f468fdd2e82bf9f8a09837
lizaShevchenko <miss.krishen80@gmail.com>
2024-12-13 15:09:59 +0200

    add DB connection manager, add schema, assign user, refactoring

---
+++ src/main/resources/sql/init.sql
@@ -0,0 +1,31 @@
+CREATE SCHEMA IF NOT EXISTS libSchema;
+
+CREATE USER libUser WITH ENCRYPTED PASSWORD 'libPwd';
+
+GRANT ALL ON SCHEMA libSchema TO libUser;
+
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA libSchema TO libUser;
+
+CREATE TABLE libSchema.author(
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(500),
+    last_name VARCHAR(500),
+    email VARCHAR(500) NOT NULL
+);
+
+CREATE TABLE libSchema.book(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(500),
+    count_pages INT,
+    author_id INT,
+    FOREIGN KEY(author_id)
+        REFERENCES libSchema.author
+);
+
+CREATE TABLE libSchema.journal(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(500),
+    count_pages INT,
+    number INT,
+    publication_year INT
+);
